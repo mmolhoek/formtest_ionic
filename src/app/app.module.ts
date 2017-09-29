8aa76c7fa8aadd21a67b48b2943fb5b066494d8a
Mischa Molhoek <mischamolhoek@gmail.com>
2017-09-29 16:33:02 +0200

    added counter for keyboard.onKeyboardShow and onKeyboardHide events, to show the first onKeyboardShow event is not triggered

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Keyboard } from '@ionic-native/keyboard';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -23,6 +23,7 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    Keyboard,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnDestroy } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { Subscription } from 'rxjs';
+import { Keyboard } from '@ionic-native/keyboard';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+export class HomePage implements OnDestroy {
+  public keyboardShowCounter: number = 0;
+  public keyboardHideCounter: number = 0;
+  private keyboardShowSubscription: Subscription;
+  private keyboardHideSubscription: Subscription;
+  constructor(public navCtrl: NavController, private platform: Platform, private keyboard: Keyboard) {
+    this.platform.ready().then(() => {
+      if (this.platform.is('cordova')) {
+        this.keyboardShowSubscription = this.keyboard.onKeyboardShow().subscribe(()=>{
+            this.keyboardShowCounter++;
+        })
+        this.keyboardHideSubscription = this.keyboard.onKeyboardHide().subscribe(()=>{
+            this.keyboardHideCounter++;
+        })
+      }
+    })
   }
+    public ngOnDestroy(): void {
+        this.keyboardShowSubscription.unsubscribe();
+        this.keyboardHideSubscription.unsubscribe();
+    }
 
 }
